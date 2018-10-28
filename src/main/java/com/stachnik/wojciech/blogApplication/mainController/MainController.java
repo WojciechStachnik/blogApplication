e96b873ed206f1eb9b7ced4f671f9f1f33db7b6d
WojciechStachnik <wojciech.stachnik@gmail.com>
2018-10-28 12:55:08 +0100

    add getters seters controller

--- src/main/java/com/stachnik/wojciech/blogApplication/mainController/MainController.java
+++ src/main/java/com/stachnik/wojciech/blogApplication/mainController/MainController.java
@@ -1,10 +1,38 @@
 package com.stachnik.wojciech.blogApplication.mainController;
 
+import com.stachnik.wojciech.blogApplication.model.Post;
+import com.stachnik.wojciech.blogApplication.repository.IPostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-    public String getIndexPage(){
+
+    @Autowired
+    IPostRepository iPostRepository;
+
+    @GetMapping("/")
+    public String getIndexPage(Model model){
+        model.addAttribute("name", "Wojciech");
         return "index";
     }
+
+    @GetMapping("/addPost")
+    public String addPostPage(){
+        return "addPost";
+    }
+
+    @PostMapping("/addPost")
+    public String addPost(@RequestParam (value = "title") String title, @RequestParam (value = "content")String content){
+        Post post = new Post(title, content);
+        iPostRepository.save(post);
+        System.out.println("Paramiters: " + title + "second: " + content);
+        return "index";
+    }
+
+
 }
--- src/main/java/com/stachnik/wojciech/blogApplication/model/Post.java
+++ src/main/java/com/stachnik/wojciech/blogApplication/model/Post.java
@@ -1,9 +1,15 @@
 package com.stachnik.wojciech.blogApplication.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@Getter
+@Setter
 public class Post {
 
     @Id
@@ -14,4 +20,9 @@ public class Post {
 
     @Temporal(TemporalType.TIMESTAMP)
     private Date added = new Date();
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
